1e8c61e1fce3cd002e57993adc3e1ad416d19333
Ito-0131 <berufegoal@yahoo.co.jp>
2023-08-13 21:19:23 +0900

    appleをlemon、cherryをgrapefruitに変更

--- src/FruitsSample.java
+++ src/FruitsSample.java
@@ -3,8 +3,8 @@ import java.util.stream.Collectors;
 
 public class FruitsSample {
     public static void main(String[] args) {
-        List<String> fruits =List.of("apple","watermelon","banana","cherry");
-        List<String> filteredFruits =fruits.stream().filter(name -> name.contains("e")).collect(Collectors.toList());
+        List<String> fruits = List.of("lemon", "watermelon", "banana", "grapefruit");
+        List<String> filteredFruits = fruits.stream().filter(name -> name.contains("e")).collect(Collectors.toList());
         filteredFruits.forEach(System.out::println);
     }
 }
